c6d0a07e5753ba235633c929ad9780e0e558222b
Valentin Placido <valentinplacido25@gmail.com>
2020-01-17 04:10:01 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -40,5 +40,9 @@ def total_even_pairs(src)
   i = 0 
   while i < src.length do 
     if (src[i][0]%2==0 && src[i][1]%2==0)
-      total += 
+      total += src[i][0] + src[i][0]
+    end
+    i+=1 
+  end
+  total
 end
